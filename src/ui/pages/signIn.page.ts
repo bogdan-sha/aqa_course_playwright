f6f1bea5f644a253967580d71b83bcad79df1eed
bshamukou <mr.shamukov@gmail.com>
2025-06-05 23:14:42 +0200

    HW-24: Remove privates for signIn.page.ts

--- src/ui/pages/signIn.page.ts
+++ src/ui/pages/signIn.page.ts
@@ -1,10 +1,10 @@
 import {SalesPortalPage} from "./salesPortal.page";
 
 export class SignInPage extends SalesPortalPage {
-    private readonly emailInput = this.page.locator('#emailinput');
-    private readonly passwordInput = this.page.locator('#passwordinput');
-    private readonly loginButton = this.page.getByRole('button', { name: 'Login'});
-    private readonly rememberMeCheckbox = this.page.getByRole('checkbox', { name: 'Remember me' });
+    readonly emailInput = this.page.locator('#emailinput');
+    readonly passwordInput = this.page.locator('#passwordinput');
+    readonly loginButton = this.page.getByRole('button', { name: 'Login'});
+    readonly rememberMeCheckbox = this.page.getByRole('checkbox', { name: 'Remember me' });
 
     uniqueElement = this.rememberMeCheckbox;
 
